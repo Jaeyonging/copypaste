85da0eea8826ea6b1c1a96b6a89b220727e93210
jaeyonging <wodyd1318@naver.com>
2025-11-11 16:17:40 +0900

    delete useless files and add user can type tags and see the preview

--- src/api/fetch.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-
-export const fetchComments = async () => {
-    const response = await axios.get("https://jsonplaceholder.typicode.com/comments");
-    return response.data;
-};
--- src/component/Clipboard.tsx
+++ src/component/Clipboard.tsx
@@ -1,14 +1,30 @@
-import React from 'react'
-import ClipboardHeader from './ClipboardHeader';
-import ClipboardFooter from './ClipboardFooter';
-
-const summary = '<div style="background-color: red; display: flex; justify-content: center; align-items: center; height: 100%; width: 100%;">글 내용입니다.</div>';
+import React, { useState } from 'react'
+import ClipboardHeader from './ClipboardHeader'
+import ClipboardFooter from './ClipboardFooter'
 
 const Clipboard = () => {
+    const [summary, setSummary] = useState(
+        `<div style="background-color: red; display: flex; justify-content: center; align-items: center; height: 100%; width: 100%;">
+      글 내용입니다.
+    </div>`
+    )
+
     return (
         <div className='w-[300px] min-h-[500px] flex flex-col justify-between border-[1px] border-black rounded-md p-2'>
-            <ClipboardHeader summary={summary}/>
-            <div className='mt-[10px]' dangerouslySetInnerHTML={{ __html: summary }} />
+            <ClipboardHeader summary={summary} />
+
+            <textarea
+                value={summary}
+                onChange={(e) => setSummary(e.target.value)}
+                className='w-full h-[150px] border rounded-md p-2 text-sm font-mono mt-2 resize-none'
+                placeholder='여기에 HTML 작성...'
+            />
+
+            <div
+                className='mt-[10px] border-t pt-2'
+                dangerouslySetInnerHTML={{ __html: summary }}
+            />
+
             <ClipboardFooter summary={summary} />
         </div>
     )
